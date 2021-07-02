edee577b00d54b04c01a0dc2879d71420e2ead22
NaNdalal-dev <dnandalal7@gmail.com>
2021-07-02 19:03:28 +0530

    Added Files

--- predict_covid_site/predict_covid_site/settings.py
+++ predict_covid_site/predict_covid_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-d@11e_x4*%hnci5bdw1&kw(nn2^bsbf-wo4fk&$xc(1f247gd*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','https://dr-covid.herokuapp.com/','dr-covid.herokuapp.com/','0.0.0.0','127.0.0.1']
 
 
 # Application definition
@@ -118,6 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static'),
+    ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
